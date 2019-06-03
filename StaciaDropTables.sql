b4c2306ec22701c981808f0313b5acb383fcd9e5
cp-jwu130 <43765758+cp-jwu130@users.noreply.github.com>
2019-06-03 16:39:45 -0700

    Added Drop Tables and updated script for Making Tables.

---
+++ StaciaDropTables.sql
@@ -0,0 +1,11 @@
+drop table clubSchedule;
+drop table clubOfficers;
+drop table clubExtraDump;
+drop table clubPresidentContact;
+drop table clubAdvisorContact;
+drop table club;
+
+drop table tutorClasses;
+drop table tutorSchedule;
+drop table tutorExtraDump;
+drop table tutor;
--- StaciaInnoDB.sql
+++ StaciaInnoDB.sql
@@ -1,77 +1,61 @@
 create table if not exists club(
-  cid integer auto_increment,
   cname varchar(200),
+  clubDescription varchar(3000),
+  website varchar(3000),
   department varchar(200),
-  primary key (cid)) engine=InnoDB;
-
-create table if not exists positions(
-  pid integer auto_increment,
-  title varchar(200) not null,
-  primary key (pid)) engine=innodb;
+  generalPhone varchar(200),
+  generalEmail varchar(200),
+  primary key (cname)) engine=InnoDB;
 
 create table if not exists clubSchedule(
   schid integer auto_increment,
-  cid integer not null,
+  cname varchar(200) not null,
   dayOfTheWeek varchar(200),
   time varchar(200),
   location varchar(200),
   primary key (schid),
-  foreign key (cid)
-    references club(cid)
+  foreign key (cname)
+    references club(cname)
     on delete cascade) engine=InnoDB;
 
 create table if not exists clubOfficers(
   cofid integer auto_increment,
-  cid integer not null,
-  pid integer not null,
+  cname varchar(200) not null,
+  title varchar(200) not null,
   name varchar(200) not null,
   term varchar(200) not null,
   primary key (cofid),
-  foreign key (cid)
-    references club(cid)
-    on delete cascade,
-  foreign key (pid)
-    references positions(pid)
+  foreign key (cname)
+    references club(cname)
     on delete cascade) engine=InnoDB;
 
 create table if not exists clubExtraDump(
   extraid integer auto_increment,
-  cid integer not null,
+  cname varchar(200) not null,
   keywords varchar(200) not null,
   data varchar(200) not null,
   primary key (extraid),
-  foreign key (cid)
-    references club(cid)
-    on delete cascade) engine=InnoDB;
-
-create table if not exists clubInfo(
-  cid integer,
-  clubDescription varchar(3000),
-  website varchar(3000),
-  generalEmail varchar(200),
-  generalPhone varchar(200),
-  primary key (cid),
-  foreign key (cid)
-    references club(cid)
+  foreign key (cname)
+    references club(cname)
     on delete cascade) engine=InnoDB;
 
 create table if not exists clubPresidentContact(
-  cid integer,
+  cname varchar(200),
   email varchar(200),
   phone varchar(200),
-  primary key (cid),
-  foreign key (cid)
-    references club(cid)
+  primary key (cname),
+  foreign key (cname)
+    references club(cname)
     on delete cascade) engine=InnoDB;
 
 create table if not exists clubAdvisorContact(
-  cid integer,
+  cname varchar(200),
   name varchar(200) not null,
   email varchar(200),
   phone varchar(200),
-  primary key (cid),
-  foreign key (cid)
-    references club(cid)
+  primary key (cname),
+  foreign key (cname)
+    references club(cname)
     on delete cascade) engine=InnoDB;
 
 create table if not exists tutor(
