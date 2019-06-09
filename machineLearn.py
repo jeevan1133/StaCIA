a8b026cc08e4bf9be5916b3b4d29876484c1d12d
Suhas Panthari <panthari.s4@gmail.com>
2019-06-08 20:15:38 -0700

    Update machineLearn.py

--- machineLearn.py
+++ machineLearn.py
@@ -90,37 +90,3 @@ while testStr != "quit":
    print(getImpTerms(testStr))
    print(classifier.classify(run(testStr)))
    testStr = input("Please input string: ")
-   
-#splStr = testStr.split(' ')
-#newList = []
-#for word in splStr:
-#   for val in varDict:
-#      if word in varDict[val]:
-#         newList.append((val, word))
-#print(run(testStr))
-#print(newList)
-#print(classifier.classify(run(testStr)))
-
-#   for i in range(len(rqSplit)):
-#      nameList = []
-#      print(rqSplit[i])
-#      if (rqSplit[i][0] == '['):
-#         offSet = 1
-#         if (rqSplit[i][len(rqSplit[i])-1] == '?' or rqSplit[i][len(rqSplit[i])-1] == '.'):
-#            offSet = 2
-#         for name in varDict[rqSplit[i][1:len(rqSplit[i])-offSet]]:
-#            nameList = list(rqSplit)
-#            nameList[i] = name
-#            for j in range(i, len(nameList)):
-#               if (nameList[j][0] == '['):
-#                  offSet = 1
-#                  if (rqSplit[j][len(rqSplit[j])-1] == '?' or rqSplit[j][len(rqSplit[j])-1] == '.'):
-#                     offSet = 2
-#                  for var in varDict[nameList[j][1:len(nameList[j])-offSet]]:
-#                     nameList[j] = var
-#                     Questlist.append(' '.join(nameList)) 
-#                     hasTwo = True
-#            if hasTwo == False:
-#               Questlist.append(' '.join(nameList))
-#               hasTwo = False
-#print(Questlist)
