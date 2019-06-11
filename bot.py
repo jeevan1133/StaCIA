0a0ddaf2e3225b236a3d7535b9d5fc3228c1326f
jeevan1133 <jeevan.basnet@selu.edu>
2019-06-10 17:36:15 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,30 +3,42 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from project3 import *
-qFile = open("Question.txt", 'r')
-from machineLearn.py import *
+from machineLearn import *
+import string
+from random import *
+
+
+
 url = "https://api.groupme.com/v3/groups?token=c7GHQhPX92iPvzNLOuLQOZpFh3e6krkgBYzfGMMp"
 r = requests.get(url)
 data = r.json()
-import string
-from random import *
 min_char = 8
 max_char = 12
+
 allchar = string.ascii_letters + string.punctuation + string.digits
+
+
 password = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
+
+
 data = {
    "bot_id"  :  "462a2b8d192fbe66e261cd8d05",
    "text"  :   "Hello world"
 }
+
 pars = {
    "limit"   :   1
    #"since_id": "462a2b8d192fbe66e261cd8d05"
 }
+
+
 clf = classTrain()
 messUrl = "https://api.groupme.com/v3/groups/51012574/messages?token=c7GHQhPX92iPvzNLOuLQOZpFh3e6krkgBYzfGMMp"
 postUrl = "https://api.groupme.com/v3/bots/post"
 r = 0
 estVal = ""
+
+
 while(True):
    r =requests.get(messUrl, params = pars)
    #print(r)
